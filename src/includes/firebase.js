e58a42ee76c2909057f8538a44f458e5ce090d28
k.chepurin <k.chepurin@anketolog.ru>
2022-10-31 15:18:13 +0700

    first commit

---
+++ src/includes/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCJkFGAvPWGjGhpgTbKwTJjTOUBYr7k_cE',
+  authDomain: 'music-558c0.firebaseapp.com',
+  projectId: 'music-558c0',
+  storageBucket: 'music-558c0.appspot.com',
+  messagingSenderId: '1058775439807',
+  appId: '1:1058775439807:web:288d78f516019d23caa90a',
+};
+
+export default firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const usersCollection = db.collection('users');
+
+export { auth, db, usersCollection };
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,49 @@
 import { createStore } from 'vuex';
+import { auth, usersCollection } from '@/includes/firebase';
 
 export default createStore({
   state: {
+    showAuthApp: false,
+    userLoggedIn: false,
   },
   mutations: {
+    toggleAuthApp: (state) => {
+      state.showAuthApp = !state.showAuthApp;
+    },
+
+    toggleAuth(state) {
+      state.userLoggedIn = !state.userLoggedIn;
+    },
   },
+
+  getters: {
+    showAuthApp: (state) => state.showAuthApp,
+  },
+
   actions: {
-  },
-  modules: {
+    async register({ commit }, payload) {
+      const userCred = await auth.createUserWithEmailAndPassword(
+        payload.email,
+        payload.password,
+      );
+      await usersCollection.doc(userCred.user.uid).set({
+        name: payload.name,
+        email: payload.email,
+        age: payload.age,
+        country: payload.country,
+      });
+
+      await userCred.user.updateProfile({
+        displayName: payload.name,
+      });
+      commit('toggleAuth');
+    },
+
+    init_login({ commit }) {
+      const user = auth.currentUser;
+      if (user) {
+        commit('toggleAuth');
+      }
+    },
   },
 });

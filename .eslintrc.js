0af177527fe7efb49d009cec7d3004c9c456e749
Kostya <kostyan3110@mail.ru>
2022-11-13 21:09:05 +0700

    rule for linebreak windows

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,44 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ['plugin:vue/vue3-essential', '@vue/airbnb'],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'max-len': ['error', { code: 250 }],
-    'object-curly-newline': [
-      'error',
-      {
-        ObjectExpression: { consistent: true, multiline: true },
-        ObjectPattern: { consistent: true, multiline: true },
-        ImportDeclaration: 'never',
-        ExportDeclaration: { multiline: true, minProperties: 3 },
-      },
-    ],
-    quotes: ['error', 'single'],
-    'operator-linebreak': [
-      'error',
-      'after',
-      {
-        overrides: {
-          ':': 'before',
-        },
-      },
-    ],
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
+    root: true,
+    env: {
+        node: true,
     },
-  ],
+    extends: ['plugin:vue/vue3-essential', '@vue/airbnb'],
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'max-len': ['error', { code: 250 }],
+        'object-curly-newline': [
+            'error',
+            {
+                ObjectExpression: { consistent: true, multiline: true },
+                ObjectPattern: { consistent: true, multiline: true },
+                ImportDeclaration: 'never',
+                ExportDeclaration: { multiline: true, minProperties: 3 },
+            },
+        ],
+        quotes: ['error', 'single'],
+        'linebreak-style': ['error', 'windows'],
+        'operator-linebreak': [
+            'error',
+            'after',
+            {
+                overrides: {
+                    ':': 'before',
+                },
+            },
+        ],
+    },
+    overrides: [{
+        files: [
+            '**/__tests__/*.{j,t}s?(x)',
+            '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        ],
+        env: {
+            jest: true,
+        },
+    }, ],
 };
